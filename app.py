8820e987339b65995b61b02b4ec4a22404728952
Aviral Garg <gargaviral99@gmail.com>
2025-02-21 07:14:02 +0000

    `Update app.py and requirements.txt to install torch and torchmcubes via pip commands`

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from tsr.bake_texture import bake_texture
 
 # Initialize Flask app
 app = Flask(__name__)
+os.system("pip install --no-cache-dir torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu")
 os.system("pip install git+https://github.com/tatsy/torchmcubes.git")
 # Configure logging
 logging.basicConfig(level=logging.INFO)
